bd03ffdb0cadcb6294c0773a9b156fe75a4849c0
Cosmin Borsan <borsancosmin07@gmail.com>
2025-01-18 10:22:01 +0200

    added prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    integrations: [tailwind()],
+    base: 'https://github.com/cosmologus/work-schedule-astro.git',
 });
+
