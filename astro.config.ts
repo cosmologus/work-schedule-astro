ccc280f10f514e0503c66430c177e57f5a5a4c54
Cosmin Borsan <borsancosmin07@gmail.com>
2025-02-05 20:16:55 +0200

    removed current day for now

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), alpinejs()],
     site: 'https://cosmologus.github.io',
     base: 'work-schedule-astro',
 });
-
